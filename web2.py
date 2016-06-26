138c363fba06240055d25c4cfcece5aa066df7a7
Lionel Messi <prithvijeet.y@gmail.com>
2016-06-26 05:17:34 -0400

    Hella updates

--- web2.py
+++ web2.py
@@ -4,7 +4,9 @@ import random
 
 
 app = Flask(__name__)
-
+  
+#global for session data, sessions are sometimes not persistent
+gSessionData ={}
 # make matrix of strings and append to it str(int) str(double) str(char) str(date)
 # then we can simple rotate it to produce the file
 def rand_str(length):
@@ -15,23 +17,47 @@ def rand_str(length):
     return id
 
 def int_generator(num_rows,col_num):
-    session[col_num] = ""
+    session[col_num] = []
     for x in range(int(num_rows)):
         num = random.randint(-2**31,2**31)
-        #session["csv"] = [[0 for x in range(4)] for y in range(int(num_rows))] 
-        #session["csv"][0][x] = num
-        session[col_num] +=(str(num) +" ")
+        #session[col_num] +=(str(num) +" ")
+        session[col_num].append(str(num))
+
+    gSessionData[col_num]  =session[col_num]
 
 def str_generator(num_rows,col_num):
-    session[col_num] = ""
+    session[col_num] = []
     for x in range(int(num_rows)):
         r_str = rand_str(5)
-        session[col_num] += r_str+ " "
+        session[col_num].append(r_str)
+
+    gSessionData[col_num]  =session[col_num]
+
+
 def dbl_generator(num_rows,col_num):
-    session[col_num] = ""
+    session[col_num] = []
     for x in range(int(num_rows)):
         num = random.uniform( -2**31, 2**31 )
-        session[col_num] +=(str(num) + " ")
+        session[col_num].append(str(num))
+
+    gSessionData[col_num]  =session[col_num]
+
+
+def session_print(col_num):
+    print "IN SESSION"
+    print ("Size is ",col_num)
+    for x in range(col_num):
+            try:
+                print session[str(x)]
+            except:
+                pass
+
+
+###############################################################################################################
+
+############################################# MAIN ROUTES #####################################################
+
+
 
 @app.route('/', methods = ['GET','POST'])
 def file_setup():
@@ -52,10 +78,16 @@ def file_setup():
                 pass
             col_num +=1
 
-        print("PRINTING COLS HERE")
-        for x in range(col_num):
-            print session[x]
-        #return redirect (url_for("file_out"))
+        session["col_num"] = col_num
+        # print("PRINTING COLS HERE")
+
+        # for x in range(col_num):
+        #     try:
+        #         print session[x]
+        #     except:
+        #         pass
+        #session_print(session["col_num"])
+        return redirect (url_for("file_out"))
 
     return render_template("home.html") #html for page
 
@@ -66,17 +98,48 @@ def file_setup():
 def file_out():
     # We need to modify the response, so the first thing we
     # need to do is create a response out of the CSV string
-    csv = (session["csv"])
-    response = make_response(csv)
+    # 2D list
+    elements = []
+    elements.append([])
+    elements.append([])
+
+
+    print "in last route"
+    trylist = []
+    #ession_print(session["col_num"])
+    try:
+        print ("HIHIH",gSessionData)
+    except:
+        pass
+    
+    # trylist = gSessionData[0]
+    # print trylist
+    # print trylist[0]
+    
+    print gSessionData
+
+    elements[0] = gSessionData[0]
+    elements[1] = gSessionData[1]
+    print("NOW PRINITNG FINAL")
+    print elements[0]
+    print elements[1]
+    
+    csv = ""
+    for key in elements[0]:
+        csv += key +" "
+    print("CSV IS ", csv)
+
+    return "HI"
+    #response = make_response(csv)
     # This is the key: Set the right header for the response
     # to be downloaded, instead of just printed on the browser
-    response.headers["Content-Disposition"] = "attachment; filename=response.txt"
-    return response
+    #response.headers["Content-Disposition"] = "attachment; filename=response.txt"
+    #return response
 
 
 
 if __name__ == '__main__':
-    app.secret_key = 'secretkeyhere'
+    app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
     app.run(debug = 'True')
 
 
